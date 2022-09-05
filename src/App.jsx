7855b585e91c7ba52eeea754a80aa31575cc6cc0
Hadi <abdulhadi.slow@gmail.com>
2022-09-05 19:45:10 +0500

    init code

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,32 @@
-import React from 'react';
-import {N1ED} from '@edsdk/n1ed-react';
+import React, { useState } from "react";
+import { N1ED } from "@edsdk/n1ed-react";
 
-class App extends React.Component {
+const App = () => {
+  const [value, setValue] = useState("");
+  function handleEditorChange(content, editor) {
+    setValue(content);
+    console.log("Content was updated:", content);
+  }
 
-    handleEditorChange(content, editor) {
-        console.log('Content was updated:', content);
-    };
-
-    render() {
-        return <N1ED
-            apiKey="REACDFLT"
-            initialValue="<p>N1ED react demo</p>"
-            onEditorChange={this.handleEditorChange}
-
-            init={{
-                height: 500,
-                toolbar: 'undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | outdent indent | myCustomToolbarButton',
-                setup: (editor) => {
-                    console.log("TinyMCE initialized");
-                    editor.ui.registry.addButton('myCustomToolbarButton', {
-                        text: 'My Custom Button',
-                        onAction: () => alert('Custom button clicked!'),
-                    });
-                },
-            }}
-
-        />;
-    }
-}
+  return (
+    <N1ED
+      apiKey="REACDFLT"
+      initialValue={value}
+      onEditorChange={handleEditorChange}
+      init={{
+        height: 500,
+        toolbar:
+          "undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | outdent indent | myCustomToolbarButton",
+        setup: (editor) => {
+          console.log("TinyMCE initialized");
+          editor.ui.registry.addButton("myCustomToolbarButton", {
+            text: "My Custom Button",
+            onAction: () => alert("Custom button clicked!"),
+          });
+        },
+      }}
+    />
+  );
+};
 
 export default App;
